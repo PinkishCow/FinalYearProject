b3762e3850bef8eed9527ac00bf1102426069b1c
Chris <christopherflinn@hotmail.com>
2019-11-23 21:02:47 +0000

    Initial Commit

---
+++ __main__.py
@@ -0,0 +1,22 @@
+import datetime
+import logging
+import sys
+import os
+
+logging.basicConfig(filename=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '.log', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+logger.info("Starting")
+logging.info(sys.platform)
+# whole program in here
+
+
+
+
+
+
+
+# TODO Menu System
+# TODO Face Example
+# TODO OS Recognition
+# TODO Multi System Communication
+# TODO Basic Training and detection system
---
+++ tools/config.py
@@ -0,0 +1,54 @@
+import sys
+import json
+import os
+import logging
+logger = logging.getLogger(__name__)
+
+CONFIG_FILE = ""
+
+if sys.platform == "win32":
+    CONFIG_FILE = "config_win32.json"
+elif sys.platform.startswith("linux"):
+    CONFIG_FILE = "config_linux.json"
+
+
+def verify_config():
+    if os.path.isfile(CONFIG_FILE) is False:
+        logger.critical(CONFIG_FILE + " does not exist in root folder.")
+        return False
+
+    with open(CONFIG_FILE, "r") as cfgRead:
+        try:
+            json.load(cfgRead)
+            return True
+        except ValueError as e:
+            logger.critical(CONFIG_FILE + " is not formatted properly {0}".format(e))
+            return False
+
+
+# load the json file as cfg, allows access as config.json. Stop on error.
+def start_config():
+    global cfg
+    if verify_config():
+        with open(CONFIG_FILE, "r") as cfgFile:
+            cfg = json.load(cfgFile)
+            logger.info(CONFIG_FILE + " loaded.")
+            return True
+    else:
+        logger.critical(CONFIG_FILE + "Unable to start. Error in loading configuration file.")
+        sys.exit(0)
+
+
+def reload_config():
+    global cfg
+    if verify_config():
+        del cfg
+        with open(CONFIG_FILE, "r") as cfgFile:
+            cfg = json.load(cfgFile)
+            logger.info(CONFIG_FILE + " reloaded in place.")
+            return True
+
+
+start_config()
+
+
