c604e6af962d5e1504bab49812bd49f9f76a72fc
Chris <christopherflinn@hotmail.com>
2019-11-23 21:48:03 +0000

    takePhoto example

--- examples/showCameraBasic.py
+++ examples/showCameraBasic.py
@@ -2,13 +2,13 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-frame_w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-frame_h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-fps = cap.get(cv2.CAP_PROP_FPS)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1296)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 972)
+cap.set(cv2.CAP_PROP_FPS, 42)
 
-print("Width: '{}'".format(frame_w))
-print("Height: '{}'".format(frame_h))
-print("FPS: '{}'".format(fps))
+print("Width: '{}'".format(cv2.CAP_PROP_FRAME_WIDTH))
+print("Height: '{}'".format(cv2.CAP_PROP_FRAME_HEIGHT))
+print("FPS: '{}'".format(cv2.CAP_PROP_FPS))
 
 if not cap.isOpened():
     print("Could not open the camera feed")
@@ -17,6 +17,9 @@ while cap.isOpened():
     ret, frame = cap.read()
 
     if ret:
+        height, width = frame.shape[:2]
+        M = cv2.getRotationMatrix2D((width/2, height/2), 180, 1)
+        frame = cv2.warpAffine(frame, M, (width, height))
         cv2.imshow("camera", frame)
 
         cv2.imshow("Grayscale cam", cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
