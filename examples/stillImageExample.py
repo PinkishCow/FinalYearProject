c186ff6087c5c8705e6d84f1172c14bf9ec5a655
Chris <christopherflinn@hotmail.com>
2019-11-24 03:59:02 +0000

    update gitignore

--- examples/stillImageExample.py
+++ examples/stillImageExample.py
@@ -1,8 +1,11 @@
 import time
 import picamera
 
-with picamera.PiCamera as cam:
-    cam.resolution = (1024, 768)
-    cam.start_preview()
-    time.sleep(.1)
-    cam.capture("wow.jpg")
+
+def run_still_example():
+
+    with picamera.PiCamera as cam:
+        cam.resolution = (1024, 768)
+        cam.start_preview()
+        time.sleep(.1)
+        cam.capture("wow.jpg")
