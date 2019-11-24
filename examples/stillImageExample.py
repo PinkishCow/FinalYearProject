7afd84495ee76333b2efb34b698e32d697b76159
Chris <christopherflinn@hotmail.com>
2019-11-24 04:02:12 +0000

    still example

--- examples/stillImageExample.py
+++ examples/stillImageExample.py
@@ -4,7 +4,7 @@ import picamera
 
 def run_still_example():
 
-    with picamera.PiCamera as cam:
+    with picamera.PiCamera() as cam:
         cam.resolution = (1024, 768)
         cam.start_preview()
         time.sleep(.1)
