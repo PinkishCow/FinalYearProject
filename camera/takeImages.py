0238301f8d75f45b690ae38b3f7bda8c448d3b78
Chris <christopherflinn@hotmail.com>
2019-12-15 18:28:30 +0000

    Take images

--- camera/takeImages.py
+++ camera/takeImages.py
@@ -8,16 +8,16 @@ def start():
     folder = input()
     print("Please input image count")
     count = input()
-    print("Taking {} images and saving them in a folder named {}", count, folder)
-    take_image_set(folder, count)
+    print("Taking {} images and saving them in a folder named {}".format(count, folder))
+    take_image_set(folder, int(count))
 
 
-def take_image_set(folder, count):
+def take_image_set(folder, count: int):
     with picamera.PiCamera() as cam:
         for x in range(count):
             time.sleep(.4)
             cam.resolution = (1024, 768)
             cam.start_preview()
             time.sleep(.1)
-            cam.capture(os.path.join("images", folder, count + ".jpg"))
+            cam.capture(os.path.join("images", folder, str(count) + ".jpg"))
 
