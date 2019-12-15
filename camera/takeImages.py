ab0f93e0122b36482bbb57db91ea974d87cd4ade
Chris <christopherflinn@hotmail.com>
2019-12-15 18:21:39 +0000

    still example

--- camera/takeImages.py
+++ camera/takeImages.py
@@ -3,6 +3,15 @@ import picamera
 import os
 
 
+def start():
+    print("Please input folder name")
+    folder = input()
+    print("Please input image count")
+    count = input()
+    print("Taking {} images and saving them in a folder named {}", count, folder)
+    take_image_set(folder, count)
+
+
 def take_image_set(folder, count):
     with picamera.PiCamera() as cam:
         for x in range(count):
