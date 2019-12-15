92fc930efe76f6e51e85f2adb7f797c1f28c4650
Chris <christopherflinn@hotmail.com>
2019-12-15 18:53:29 +0000

    change still example

--- examples/stillImageExample.py
+++ examples/stillImageExample.py
@@ -5,11 +5,11 @@ from picamera import PiCamera
 def run_still_example():
 
     camera = PiCamera(
-        resolution=(1280, 720),
-        sensor_mode=6
+        sensor_mode=7
     )
 
     camera.start_preview()
     time.sleep(10)
     camera.capture('test.jpg')
+    camera.stop_preview()
 
