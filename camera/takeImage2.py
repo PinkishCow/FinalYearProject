e15299aaec93b3f62ec493ff201e5d2d2ef1d46f
Chris <christopherflinn@hotmail.com>
2019-12-16 16:48:14 +0000

    More image work

--- camera/takeImage2.py
+++ camera/takeImage2.py
@@ -3,25 +3,21 @@ import picamera
 import os
 
 
-frames = 10
-folder = ""
-
-
 def start():
     print("Please input folder name")
     folder = input()
     print("Please input image count")
-    frames = input()
+    frames = int(input())
     print("Taking {} images and saving them in a folder named {}".format(frames, folder))
     folder = os.path.join("images", folder)
     if os.path.exists(folder):
-        take_image_set()
+        take_image_set(folder, frames)
     else:
         os.mkdir(folder)
-        take_image_set()
+        take_image_set(folder, frames)
 
 
-def filenames():
+def filenames(folder, frames):
     frame = 0
     while frame < frames:
         yield os.path.join(folder, "image%02d.jpg" % frame)
@@ -29,13 +25,13 @@ def filenames():
         frame += 1
 
 
-def take_image_set():
+def take_image_set(folder, frames):
     with picamera.PiCamera() as camera:
         camera.resolution = (1024, 768)
         camera.framerate = 5
         camera.start_preview()
         time.sleep(1)
         begin = time.time()
-        camera.capture_sequence(filenames(), use_video_port=True)
+        camera.capture_sequence(filenames(folder, frames), use_video_port=True)
         finish = time.time()
         print("Captured %d frames at %.2ffps" %(frames, frames / (finish - begin)))
