567f208a6d7c0af3f6ba599a47d6e1895e6e2e5f
Chris <christopherflinn@hotmail.com>
2020-05-21 12:37:13 +0100

    test server

--- recognition/cascaderecognition.py
+++ recognition/cascaderecognition.py
@@ -7,8 +7,7 @@ import numpy
 import csv
 import os
 
-# import tools.config as config
-import pyodbc
+#import pyodbc
 
 
 
@@ -90,101 +89,101 @@ class CascadeRecognition:
                             0.9, (255, 255, 0), 2)
 
 
-async def accuracy_test():
-    classifier_folder = "D:\\Desktop\\Dissertation\\images\\cas"
-    image_csv = "D:\\Desktop\\Dissertation\\images\\testingImages\\vott-csv-export\\TestingImages-export.csv"
-    image_folder = "D:\\Desktop\\Dissertation\\images\\testingImages\\vott-csv-export"
-    output_folder = "D:\\Desktop\\Dissertation\\images\\FinalResultsOutput"
-
-    recogniser_scale_values = [1.5, 1.4, 1.3, 1.25, 1.2, 1.15, 1.1, 1.05, 1.04, 1.03, 1.02, 1.01]
-    recogniser_neighbour_values = [1, 3, 5, 7, 10, 15, 20, 25, 30]
-
-    image_data_in = []
-
-    with open(image_csv, newline='') as csv_in:
-        reader = csv.reader(csv_in, quoting=csv.QUOTE_NONNUMERIC)
-        next(reader)
-        image_data_in = list(reader)
-
-    # True pos, True Neg, False Pos, False Neg, mAP, IoU>50, IoU>75
-    conn = pyodbc.connect(
-        r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\Desktop\Dissertation\images\FinalResultsCas.accdb;')
-    cursor = conn.cursor()
-    print("connected")
-    for size_name in os.listdir(classifier_folder):
-        for item_name in os.listdir(os.path.join(classifier_folder, size_name)):
-            for scale in recogniser_scale_values:
-                for nbr in recogniser_neighbour_values:
-                    recog = CascadeRecognition(scale, nbr)
-                    recog.toggle_clean(False)
-                    recog.add_classifier(os.path.join(classifier_folder, size_name, item_name, "cascade.xml"),
-                                         item_name)
-                    for image_file in os.listdir(image_folder):
-                        if not image_file.endswith(".jpg"):
-                            continue
-                        print(image_file + ", " + size_name + ", " + item_name + ", " + str(scale) + ", " + str(nbr))
-                        # Cannot have "True Negative" because every test image contains something that should be recognised
-                        true_pos = 0  # At least 1 bounding box with Iou >= 50
-                        false_pos = 0  # Bounding box with IoU < 50
-                        false_neg = 0  # No bounding box with IoU >= 50
-                        iou50 = 0
-                        iou75 = 0
-
-                        contains_item = False
-                        item_found = False
-
-                        image_items = {item_name, image_file}
-                        # https://stackoverflow.com/questions/1658505/searching-within-nested-list-in-python
-                        image_gt = None
-                        try:
-                            if next(subl for subl in image_data_in if image_items.issubset(subl)):
-                                image_gt = next(subl for subl in image_data_in if image_items.issubset(subl))
-                                contains_item = True
-                        except StopIteration:
-                            pass
-                        image = cv2.imread(os.path.join(image_folder, image_file))
-                        matches = recog.recognise(image)
-                        if contains_item:
-                            # https://www.pyimagesearch.com/2016/11/07/intersection-over-union-iou-for-object-detection/
-                            for match in matches:
-                                # 1 = xmin, 2 = ymin, 3 = xmax, 4 = ymax
-                                for (x, y, w, h) in match[1][0]:
-                                    # Intersection
-                                    xA = max(x, image_gt[1])
-                                    yA = max(y, image_gt[2])
-                                    xB = min(x + w, image_gt[3])
-                                    yB = min(y + h, image_gt[4])
-
-                                    intersection_area = max(0, xB - xA + 1) * max(0, yB - yA + 1)
-
-                                    # Union
-                                    resultArea = ((x + w) - x + 1) * ((y + h) - y + 1)
-                                    truthArea = (image_gt[3] - image_gt[1] + 1) * (image_gt[4] - image_gt[2] + 1)
-
-                                    # IoU
-                                    iou = intersection_area / float(resultArea + truthArea - intersection_area)
-
-                                    if iou >= 0.5:
-                                        iou50 += 1
-                                        if iou >= 0.75:
-                                            iou75 += 1
-                                        if not item_found:
-                                            true_pos += 1
-                                            item_found = True
-                                    else:
-                                        false_pos += 1
-                            if not item_found:
-                                false_neg += 1
-                        else:
-                            for match in matches:
-                                false_pos += 1
-
-                        cursor.execute('''
-                        INSERT INTO Results ( Filename, True_Positive, False_Positive, False_Negative, IoU50, IoU75, Cascade_Size, Cascade_Item, Cascade_Scale, Cascade_Neighbours )
-                        VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                        ''', (
-                            image_file, true_pos, false_pos, false_neg, iou50, iou75, size_name, item_name, scale, nbr))
-                        conn.commit()
+# async def accuracy_test():
+#     classifier_folder = "D:\\Desktop\\Dissertation\\images\\cas"
+#     image_csv = "D:\\Desktop\\Dissertation\\images\\testingImages\\vott-csv-export\\TestingImages-export.csv"
+#     image_folder = "D:\\Desktop\\Dissertation\\images\\testingImages\\vott-csv-export"
+#     output_folder = "D:\\Desktop\\Dissertation\\images\\FinalResultsOutput"
+#
+#     recogniser_scale_values = [1.5, 1.4, 1.3, 1.25, 1.2, 1.15, 1.1, 1.05, 1.04, 1.03, 1.02, 1.01]
+#     recogniser_neighbour_values = [1, 3, 5, 7, 10, 15, 20, 25, 30]
+#
+#     image_data_in = []
+#
+#     with open(image_csv, newline='') as csv_in:
+#         reader = csv.reader(csv_in, quoting=csv.QUOTE_NONNUMERIC)
+#         next(reader)
+#         image_data_in = list(reader)
+#
+#     # True pos, True Neg, False Pos, False Neg, mAP, IoU>50, IoU>75
+#     conn = pyodbc.connect(
+#         r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\Desktop\Dissertation\images\FinalResultsCas.accdb;')
+#     cursor = conn.cursor()
+#     print("connected")
+#     for size_name in os.listdir(classifier_folder):
+#         for item_name in os.listdir(os.path.join(classifier_folder, size_name)):
+#             for scale in recogniser_scale_values:
+#                 for nbr in recogniser_neighbour_values:
+#                     recog = CascadeRecognition(scale, nbr)
+#                     recog.toggle_clean(False)
+#                     recog.add_classifier(os.path.join(classifier_folder, size_name, item_name, "cascade.xml"),
+#                                          item_name)
+#                     for image_file in os.listdir(image_folder):
+#                         if not image_file.endswith(".jpg"):
+#                             continue
+#                         print(image_file + ", " + size_name + ", " + item_name + ", " + str(scale) + ", " + str(nbr))
+#                         # Cannot have "True Negative" because every test image contains something that should be recognised
+#                         true_pos = 0  # At least 1 bounding box with Iou >= 50
+#                         false_pos = 0  # Bounding box with IoU < 50
+#                         false_neg = 0  # No bounding box with IoU >= 50
+#                         iou50 = 0
+#                         iou75 = 0
+#
+#                         contains_item = False
+#                         item_found = False
+#
+#                         image_items = {item_name, image_file}
+#                         # https://stackoverflow.com/questions/1658505/searching-within-nested-list-in-python
+#                         image_gt = None
+#                         try:
+#                             if next(subl for subl in image_data_in if image_items.issubset(subl)):
+#                                 image_gt = next(subl for subl in image_data_in if image_items.issubset(subl))
+#                                 contains_item = True
+#                         except StopIteration:
+#                             pass
+#                         image = cv2.imread(os.path.join(image_folder, image_file))
+#                         matches = recog.recognise(image)
+#                         if contains_item:
+#                             # https://www.pyimagesearch.com/2016/11/07/intersection-over-union-iou-for-object-detection/
+#                             for match in matches:
+#                                 # 1 = xmin, 2 = ymin, 3 = xmax, 4 = ymax
+#                                 for (x, y, w, h) in match[1][0]:
+#                                     # Intersection
+#                                     xA = max(x, image_gt[1])
+#                                     yA = max(y, image_gt[2])
+#                                     xB = min(x + w, image_gt[3])
+#                                     yB = min(y + h, image_gt[4])
+#
+#                                     intersection_area = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+#
+#                                     # Union
+#                                     resultArea = ((x + w) - x + 1) * ((y + h) - y + 1)
+#                                     truthArea = (image_gt[3] - image_gt[1] + 1) * (image_gt[4] - image_gt[2] + 1)
+#
+#                                     # IoU
+#                                     iou = intersection_area / float(resultArea + truthArea - intersection_area)
+#
+#                                     if iou >= 0.5:
+#                                         iou50 += 1
+#                                         if iou >= 0.75:
+#                                             iou75 += 1
+#                                         if not item_found:
+#                                             true_pos += 1
+#                                             item_found = True
+#                                     else:
+#                                         false_pos += 1
+#                             if not item_found:
+#                                 false_neg += 1
+#                         else:
+#                             for match in matches:
+#                                 false_pos += 1
+#
+#                         cursor.execute('''
+#                         INSERT INTO Results ( Filename, True_Positive, False_Positive, False_Negative, IoU50, IoU75, Cascade_Size, Cascade_Item, Cascade_Scale, Cascade_Neighbours )
+#                         VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+#                         ''', (
+#                             image_file, true_pos, false_pos, false_neg, iou50, iou75, size_name, item_name, scale, nbr))
+#                         conn.commit()
 
 
 async def can_test():
@@ -321,4 +320,4 @@ def invalid():
 
 # Change as needed
 
-asyncio.run(accuracy_test())
+#asyncio.run(accuracy_test())
